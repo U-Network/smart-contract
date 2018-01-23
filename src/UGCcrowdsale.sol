5ad1bcde18e66a789fb61641677416ac97d55c01
Tourist1236 <guanyx2007@hotmail.com>
2018-01-22 22:06:55 -0800

    Initial version completed

--- src/UGCcrowdsale.sol
+++ src/UGCcrowdsale.sol
@@ -27,8 +27,10 @@ contract UGCcrowdSale {
     BitsupToken ugcToken;
     uint256 public raisedSoFar;
 
-    // Investor balances
+    // Investor balances in ETH 
     mapping (address => uint256) balances;
+
+    event GoalReached(address beneficiary, uint amountRaised);
 
     /**
      * Revert if sender is not beneficiary
@@ -50,6 +52,10 @@ contract UGCcrowdSale {
         return balances[_investor];
     }
 
+    /**
+     * Constructor
+     * @param _tokenAddress The addres of UGC token contract
+     */
     function UGCcrowdSale(address _tokenAddress, address _beneficiary, uint256 _start) public {
     	ugcToken = BitsupToken(_tokenAddress);
     	beneficiary = _beneficiary;
@@ -57,14 +63,84 @@ contract UGCcrowdSale {
     	end   = start + presalePeriod + preICOPeriod + ICOPeriod;
 
     }
-
-    function ETH2UGC(uint256 _wei) public pure returns (uint256 amount)  {
-    	uint256 dummy_value = _wei; // make compiler happy
-    	return dummy_value;
-    	//todo
+    /**
+     * Internal function that calculates UGC amount given the current rate
+     * @param _wei Amount of ETH to convert
+     * @return The amount of UGC 
+     */
+    function ETH2UGC(uint256 _wei) private view returns (uint256 amount)  {
+        uint256 rate = 0;
+        if(now >= start && now <= end){
+            if(now <= start + presalePeriod){
+                rate = ratePresale;
+            }
+            else if(now <= start + presalePeriod + preICOPeriod){
+                rate = ratePreICO;
+            }
+            else if(now <= start + presalePeriod + preICOPeriod + ICOPeriod){
+                rate = ICOPeriod;
+            }
+        }
+        return _wei * rate * 10**8 / 1 ether;
+        
     }
+
+    /**
+     * Receives ETH and reward the sender with UGC
+     */
     function () payable public{
-    	//todo
+
+        require(now >= start && now <= end);
+        
+        require(msg.value > minAcceptedAmount);
+
+        require(raisedSoFar + msg.value <= hardCap);
+
+        uint ugcAmount = ETH2UGC(msg.value);
+
+        require(ugcAmount != 0);
+
+        require(ugcToken.transfer(msg.sender, ugcAmount));
+
+        balances[msg.sender] += msg.value;
+
+        raisedSoFar += msg.value;
+    	
     }
 
+    /**
+     * Transfer the raised amount to foundation (Beneficiary)
+     */
+    function withdraw() public onlyBeneficiary {
+        require(raisedSoFar > softCap);
+
+        require(now > end);
+
+        //require UGC is unlocked? Are we locking the token? 
+
+        uint256 ETHbalance = this.balance;
+
+        require(beneficiary.send(ETHbalance));
+
+        GoalReached(beneficiary, ETHbalance);
+    }
+
+    /**
+     * Refund in the case of an unsuccessful crowdsale. The 
+     * crowdsale is considered unsuccessful if softCap is not reached by
+     * the end
+     */
+
+     function refund()  public {
+        require(now >= end);
+        require(raisedSoFar < softCap);
+
+        uint256 investedAmount = balances[msg.sender];
+
+        require(investedAmount > 0);
+
+        require(msg.sender.send(investedAmount));
+
+        balances[msg.sender] = 0;
+     }
 }
